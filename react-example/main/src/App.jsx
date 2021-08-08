4da6713f9d42b789e3ae4971ea469b34d668781b
Tuấn Đỗ Văn <dotuan9x@gmail.com>
2021-08-08 15:40:30 +0700

    Update react example

--- react-example/main/src/App.jsx
+++ react-example/main/src/App.jsx
@@ -5,7 +5,8 @@ const RelatedProducts = React.lazy(() => import('Products/App'));
 function App() {
     return (
         <React.Suspense fallback={null}>
-            <div className="container flex flex-row m-10 border border-gray-100 rounded">
+            <div style={{width: 1000}} className="relative container mx-auto flex flex-row m-10 border-dashed border-2 border-red-500 rounded">
+                <div className="absolute -top-7 text-red-500 font-bold">Team Core (<a target="_blank" href="https://github.com/dotuan9x/micro-frontends/tree/master/react-example/main" rel="noreferrer">main</a>) </div>
                 <div className="flex flex-col w-full p-5">
                     <div className="flex flex-row w-full justify-between">
                         <h1 className="text-lg font-bold">The Model Store</h1>
@@ -18,21 +19,23 @@ function App() {
                         <div className="w-1/3 pt-10">
                             <label className="text-lg font-medium">Tractor Porsche-Diesel Master 419</label>
                             <ul className="flex flex-row">
-                                <li>
+                                <li className="cursor-pointer border-b-2 border-white hover:border-gray-300">
                                     <img alt="" src="https://micro-frontends.org/0-model-store/images/tractor-red-thumb.jpg" />
                                 </li>
-                                <li>
+                                <li className="cursor-pointer border-b-2 border-white hover:border-gray-300">
                                     <img alt="" src="https://micro-frontends.org/0-model-store/images/tractor-green-thumb.jpg" />
                                 </li>
-                                <li>
+                                <li className="cursor-pointer border-b-2 border-white hover:border-gray-300">
                                     <img alt="" src="https://micro-frontends.org/0-model-store/images/tractor-blue-thumb.jpg" />
                                 </li>
                             </ul>
-                            <button className="border border-gray-100 rounded p-3">Buy for 66,00 $</button>
+                            <button className="font-medium hover:bg-gray-50 border border-gray-300 rounded p-3 mt-10">Buy for 66,00 $</button>
                         </div>
                     </div>
                 </div>
-                <RelatedProducts />
+                <div className="p-1">
+                    <RelatedProducts />
+                </div>
             </div>
         </React.Suspense>
     );
--- react-example/products/src/App.jsx
+++ react-example/products/src/App.jsx
@@ -4,17 +4,18 @@ import {HashRouter} from 'react-router-dom';
 function App() {
     return (
         <HashRouter>
-            <div className="border-l border-gray-100 p-5">
+            <div style={{width: 200}} className="relative border-dashed border-2 border-green-300 p-2 rounded">
+                <div className="absolute -top-8 left-0 text-green-300 font-bold">Team Product (<a target="_blank" href="https://github.com/dotuan9x/micro-frontends/tree/master/react-example/products" rel="noreferrer">products</a>) </div>
                 <h1 className="text-base font-medium">Related products</h1>
                 <ul className="flex flex-col">
                     <li className="cursor-pointer">
-                        <img alt="" src="https://micro-frontends.org/0-model-store/images/tractor-red-thumb.jpg" />
+                        <img style={{width: 180}} alt="" src="https://micro-frontends.org/0-model-store/images/tractor-red.jpg" />
                     </li>
                     <li className="cursor-pointer">
-                        <img alt="" src="https://micro-frontends.org/0-model-store/images/tractor-green-thumb.jpg" />
+                        <img style={{width: 180}} alt="" src="https://micro-frontends.org/0-model-store/images/tractor-green.jpg" />
                     </li>
                     <li className="cursor-pointer">
-                        <img alt="" src="https://micro-frontends.org/0-model-store/images/tractor-blue-thumb.jpg" />
+                        <img style={{width: 180}} alt="" src="https://micro-frontends.org/0-model-store/images/tractor-blue.jpg" />
                     </li>
                 </ul>
             </div>
